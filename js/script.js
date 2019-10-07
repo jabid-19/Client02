96a97afeee3aa3e9fd32fec9916c7abdb8852b8c
Jabid Hasan <jabid19191@gmail.com>
2019-10-07 12:13:27 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+sal();
+
+const flightPath={
+    curviness: 1.25,
+    autoRotate: true,
+    values:[
+        {x: 50, y: -20},
+        {x: 500, y: 10},
+        {x: 500, y: -50},
+        {x: 950, y: 100},
+        {x: 550, y: -50},
+        {x: 800, y: 0},
+    
+    ]
+};
+
+const tween= new TimelineLite();
+
+tween.add(
+    TweenLite.to(".plane", 1, {
+        bezier: flightPath,
+        ease: Power1.easeInOut
+    })
+);
+
+const controller= new ScrollMagic.Controller();
+
+const scene= new ScrollMagic.Scene({
+    triggerElement: ".animation",
+    duration: 2000,
+    triggerHook:0
+})
+.setTween(tween)
+// .addIndicators()
+.addTo(controller);
